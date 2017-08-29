06dc4f7843cea922c831c874fb97af490f9d4085
ChrisHansen <Chriskhansen@hotmail.com>
2017-08-29 15:36:45 +0200

    Added entities, VisualObject.

---
+++ HorsieGame/HorsieGame/Entities/Horse.py
@@ -0,0 +1,12 @@
+from Entities import VisualObject
+
+class Horse(VisualObject):
+
+    def __init__(self,x,y):
+        # Den fucking hest
+
+
+        # find fucking png
+        png = ""
+
+        super().__init__(x,y,png)
---
+++ HorsieGame/HorsieGame/Entities/VisualObject.py
@@ -0,0 +1,6 @@
+class VisualObject(object):
+
+    def __init__(self,x,y,png):
+        self.x = x
+        self.y = y
+        self.png = png
