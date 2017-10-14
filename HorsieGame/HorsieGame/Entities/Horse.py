541f5f2c656496e87f9d5f5360c70938ce02f738
ChrisHansen <Chriskhansen@hotmail.com>
2017-10-14 20:47:57 +0200

    Tilføjet Run function til Horse.

--- HorsieGame/HorsieGame/Entities/Horse.py
+++ HorsieGame/HorsieGame/Entities/Horse.py
@@ -1,12 +1,22 @@
 from Entities import VisualObject
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import interpolate
 
-class Horse(VisualObject):
 
-    def __init__(self,x,y):
+class Horse(object):
+    def __init__(self,KnotPoints,KnotValues,position):
         # Den fucking hest
+        self.KnotPoints = KnotPoints
+        self.KnotValues = KnotValues
+        self.position = position
+     
+    def Run(self):
+        tck = interpolate.splrep(self.KnotPoints,self.KnotValues,s=0)
+        self.position += interpolate.splint(0, self.KnotPoints[2], tck)
+      
 
+Tarok = Horse([0,0.5,0.67,0.98,1.2], [1,1.2,0.78,0.42,1.1], 0)
+Tarok.Run()
 
-        # find fucking png
-        png = ""
-            
-        super().__init__(x,y,png)
+print (Tarok.position)
--- HorsieGame/HorsieGame/Entities/VisualObject.py
+++ HorsieGame/HorsieGame/Entities/VisualObject.py
@@ -1,6 +1,7 @@
 class VisualObject(object):
 
-    def __init__(self,x,y,png):
+    def __init__(self,x,y,png, rotation):
         self.x = x
         self.y = y
         self.png = png
+        self.rotation = rotation
